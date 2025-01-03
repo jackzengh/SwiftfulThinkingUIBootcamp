ea3ddc321cd1e9607264848bd9367c1f969af3e7
jackzengh <jackzengh@gmail.com>
2025-01-03 20:54:51 +0100

    [Feature] Rounded Rectangle added

---
+++ SwiftfulThinkingBootcamp/TextFieldBootcamp.swift
@@ -0,0 +1,19 @@
+//
+//  TextFieldBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Jack Zheng on 2/1/2025.
+//
+
+import SwiftUI
+
+struct TextFieldBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25)
+    }
+}
+
+#Preview {
+    TextFieldBootcamp()
+}
